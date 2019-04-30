d1735a48b61bb7cafc2d7ba625bfe622bca58631
Samsul Arif Zulvian <sarizu99@gmail.com>
2019-04-30 21:45:10 +0700

    removed unnecessary script

--- resources/views/admin/post-edit.blade.php
+++ resources/views/admin/post-edit.blade.php
@@ -106,7 +106,6 @@
                                     <img src="{{ $post->thumbnail ? asset($post->thumbnail) : '#' }}" alt="post thumbnail" class="mw-100 mt-2 mb-3">
                                 </div>
                                 <div class="custom-file">
-                                    <input type="hidden" name="MAX_FILE_SIZE" value="10000000" />
                                     <input type="file" class="custom-file-input" id="customFile" name="thumbnail" accept="image/*">
                                     <label id="thumbnailLabel" class="custom-file-label" for="customFile">
                                         @if ($post->thumbnail)
